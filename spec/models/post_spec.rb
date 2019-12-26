708ae40777e9909a6bb5582c29a39dd2cf52a71e
sample <sample@samplenoMacBook-ea.local>
2019-12-26 18:56:56 +0900

    テスト実装

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  it 'contentが空ならバリデーションが通らない' do
+    post = Post.new(content: '')
+    expect(post).not_to be_valid
+  end
+
+  it 'contentに内容が記載されていればバリデーションが通る' do
+    post = Post.new(content: '成功')
+    expect(post).to be_valid
+  end
+end 
