ef17e6326c12af4fab7f5b3855ee1030b49e7dbc
sample <sample@samplenoMacBook-ea.local>
2019-12-27 15:09:34 +0900

    heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,17 +18,3 @@
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
-
-const btn = document.querySelector("#btn-mode");
-
-btn.addEventListener("change", () => {
-  if (btn.checked == true) {
-
-    document.body.classList.remove("light-theme");
-    document.body.classList.add("dark-theme");
-  } else {
-
-    document.body.classList.remove("dark-theme");
-    document.body.classList.add("light-theme");
-  }
-});
